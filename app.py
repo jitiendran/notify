4c8e9062943bbaeff478ba946e2955256990938e
Jitiendran <jitiendranjiji2000@gmail.com>
2021-04-09 10:40:40 +0530

    added session

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,redirect
+from flask.globals import session
 from flask.helpers import url_for
 from flask.templating import render_template
 from datetime import date
@@ -10,15 +11,14 @@ app = Flask(__name__,template_folder='main')
 
 
 url = 'mongodb+srv://jiji:123@cluster0.md3ui.mongodb.net/test'
-logged = False
 client = pymongo.MongoClient(url)
 Database = client.get_database('notify')
+
 if Database : 
     print("!!database connected!!")
 
-Username = 'name'
-UserTable = Database.UserTable
-TaskTable = Database.TaskTable
+UserTable = Database['UserTable']
+TaskTable = Database['TaskTable']
 
 @app.route('/')
 def index() :
@@ -37,7 +37,6 @@ def signup():
         'Email' : request.form['Email'],
         'Password' : request.form['Password']
     }
-    #creating a collection on username for storing tasks
     query = UserTable.insert_one(queryObject)
     print(request.form['Username'])
     return redirect(url_for('User',name = request.form['Username']))
@@ -54,10 +53,8 @@ def login() :
 
 @app.route('/tasks',methods=['POST'])
 def createTasks():
-    print('Username : ',Username)
-    user = UserTable.find_one({"Username": Username})
+    user = UserTable.find_one({"Username": session['Username']})
     obj = json.loads(json_util.dumps(user))
-    print('The object is : ',obj)
     queryObject = {
         'Task' : request.form['Task'],
         'Date' : request.form['Date'],
@@ -65,15 +62,14 @@ def createTasks():
         'IsSent' : False
     }
     query = TaskTable.insert_one(queryObject)
-    return redirect(url_for('User',name=Username))
+    return redirect(url_for('User',name=session['Username']))
 
 
 @app.route('/User',methods=['GET'])
 def User() :
     today = date.today()
     user = request.args.get('name')
-    global Username
-    Username = user
+    session['Username'] = user
     query = TaskTable.find({"Date": str(today)})
     obj = json.loads(json_util.dumps(query))
     task = []
@@ -88,7 +84,7 @@ def delete():
     task = request.args.get('task')
     query = TaskTable.delete_one({"Task": task})
     print('Hello this is delete')
-    return redirect(url_for('User',name=Username))
+    return redirect(url_for('User',name=session['Username']))
 
 
 if __name__ == '__main__' :
